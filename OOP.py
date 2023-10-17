28a5b258e1154c301241daef1b082b8f887043be
stully18 <147019544+stully18@users.noreply.github.com>
2023-10-17 15:33:40 -0400

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,8 @@
+from OOP_Class import Car
+
+car_1 = Car('Chevy','Corvette,',2021,'Blue')
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+print(car_1.color)
---
+++ binary converter.py
@@ -0,0 +1,3 @@
+a = int(input('What number would you like to see in binary? '))
+binary = str(bin(a))
+print(f'{a} in binary is {binary[2:]}')
